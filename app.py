16adf6d7f917580a8b28b89af77b4d92a7aa2835
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2021-11-10 23:01:16 +0500

    add functions to pages

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def helindexlo():
+def index():
     return render_template('index.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
