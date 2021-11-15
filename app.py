3eeeffbb0f27aef10dfd1ec7b99446189d4396c9
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2021-11-15 21:34:07 +0500

    dunno

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ def index():
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title = 'Contact')
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    # return render_template('about.html', title = 'About')
+    return "<h1 style='color: red;'>About Page</h1>"
 
 @app.route("/projects")
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', title = 'Projects')
 
