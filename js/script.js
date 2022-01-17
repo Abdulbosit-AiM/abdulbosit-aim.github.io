4fd03ca8184bd76a68ce1bd92bba7f2beab415a6
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-01-17 13:47:28 +0500

    + hide & delete functions

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const heading = document.querySelector('h1')
+const heading2 = document.querySelector('h2')
+const main = document.querySelector('main')
+
+heading.onclick = () => { hide(heading) }
+heading2.onclick = () => { hide(heading2) }
+
+function hide(elem){
+    elem.onclick = () => {elem.style.visibility = 'hidden'}
+}
+
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        main.innerHTML = ''
+    }
+}
